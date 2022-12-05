beb416bfa6678faf1e1aa801c5c442d3d0b0dfce
df <dufan@vspn.com>
2022-12-05 14:20:51 +0800

    字符串切片猜想验证

--- md/slice_test.go
+++ md/slice_test.go
@@ -1,7 +1,9 @@
 package md
 
 import (
+	"reflect"
 	"testing"
+	"unsafe"
 )
 
 // growSlice 切片扩容新容量计算规则
@@ -42,4 +44,36 @@ func TestGrowSlice(t *testing.T) {
 			t.Logf("%d\n", growSlice(arg.oldCap, arg.cap))
 		})
 	}
+}
+
+// TestStringToByte 测试字符串转字节切片有没有将结束标记'0'带出来
+func TestStringToByte(t *testing.T) {
+	s := "HELLO"
+	t.Logf("s(字符串)长度：%d\n", len(s))
+	t.Logf("s(字符串)内容：%s\n", s)
+
+	stringHeader := (*reflect.StringHeader)(unsafe.Pointer(&s))
+
+	var b []byte
+	pBytes := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	pBytes.Data = stringHeader.Data
+	pBytes.Len = stringHeader.Len
+	pBytes.Cap = stringHeader.Len
+	t.Logf("b(切片)长度：%d\n", len(b))
+	t.Logf("b(切片)内容：%s\n", b)
+
+	var b1 = make([]byte, len(b))
+	_ = copy(b1, b)
+	t.Logf("b1(切片)长度：%d\n", len(b1))
+	t.Logf("b1(切片)内容：%s\n", string(b1))
+
+	bytesHeader := (*reflect.SliceHeader)(unsafe.Pointer(&b1))
+
+	var s1 string
+	pStr := (*reflect.StringHeader)(unsafe.Pointer(&s1))
+	pStr.Data = bytesHeader.Data
+	pStr.Len = bytesHeader.Len
+
+	t.Logf("s1(字符串)长度：%d\n", len(s1))
+	t.Logf("s1(字符串)内容：%s\n", s1)
 }
